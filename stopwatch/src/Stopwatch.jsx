d486ddf9cb740d58077941df006720e62e82d86f
Kumar Harshvardhan <sharshu1999hraj@gmail.com>
2024-06-13 06:20:35 +0530

    STopwatch Functionality completed

--- stopwatch/src/Stopwatch.jsx
+++ stopwatch/src/Stopwatch.jsx
@@ -1,16 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
-const Stopwatch =() =>{
+const Stopwatch = () => {
+  const [time, setTime] = useState(0);
+  const [running, setRunning] = useState(false);
 
-    return(
-        <>
-        <h1>Stopwatch</h1>
-        <span>Time: 0:00</span><br /><br />
-        <div>
-            <button>Stop</button> 
-            <button>Reset</button>
-        </div>
-        </>
-    )
-}
+  const startTimer = () => {
+    setRunning(true);
+  };
+
+  const stopTimer = () => {
+    setRunning(false);
+  };
+
+  const resetTimer = () => {
+    setTime(0);
+    setRunning(false);
+  };
+
+  useEffect (() => {
+    let timerId;
+     timerId = running 
+        ? setInterval(() => {
+            setTime(prevTime => prevTime + 1);
+          }, 1000)
+        : clearInterval(timerId);
+
+    return () => clearInterval(timerId);
+
+  },[running])
+
+  const formatTime = time => {
+    const minutes = Math.floor((time % 3600) / 60);
+    const seconds = time % 60;
+
+    const formatNumber = num => (num < 10 ? `0${num}` : num);
+
+    return `${minutes}:${formatNumber(seconds)}`;
+  };
+
+  return (
+    <>
+      <h1>Stopwatch</h1>
+      <span>Time : {formatTime(time)}</span> <br /> <br />
+      {!running ? (
+        <button onClick={startTimer}>Start</button>
+      ) : (
+        <button onClick={stopTimer}>Stop</button>
+      )}
+      <button onClick={resetTimer}>Reset</button>
+    </>
+  );
+};
 export default Stopwatch;
